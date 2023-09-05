2f475ee8fcd02df7831ca32255369da63785bed6
mecatheclau <maniragabaclaude@gmail.com>
2023-09-05 20:57:05 +0200

    data

--- mosaic_app/admin.py
+++ mosaic_app/admin.py
@@ -13,20 +13,20 @@ from .models import *
 @admin.register(Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
     list_display = ('brand', 'car_model', 'year', 'plate')
-    list_filter = ('brand', 'year')
+    # list_filter = ('brand', 'year')
     search_fields = ('brand', 'plate')
     
     
 @admin.register(VehicleMaintenance)
 class VehicleMaintenanceAdmin(admin.ModelAdmin):
     list_display = ('vehicle', 'maintenance_date', 'service_type')
-    list_filter = ('vehicle', 'maintenance_date')
+    # list_filter = ('vehicle', 'maintenance_date')
     
     
 @admin.register(FundManagement)
 class FundManagementAdmin(admin.ModelAdmin):
     list_display = ('label', 'transaction_date', 'account_number', 'total')
-    list_filter = ('label', 'transaction_date')
+    # list_filter = ('label', 'transaction_date')
     
 
 @admin.register(Chat)
@@ -117,7 +117,7 @@ class ChatAdmin(admin.ModelAdmin):
 @admin.register(Staff)
 class StaffAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'position')
-    list_filter = ('position', 'sex')
+    # list_filter = ('position', 'sex')
     
 class ItemInline(admin.TabularInline):
     model = Item
@@ -128,7 +128,7 @@ class ItemInline(admin.TabularInline):
 @admin.register(Invoice) 
 class InvoiceAdmin(admin.ModelAdmin):
     list_display = ('invoice_id', 'invoice_type', 'invoice_location')
-    list_filter = ('invoice_type', )
+    # list_filter = ('invoice_type', )
     
     inlines=[ItemInline]
     
